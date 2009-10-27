06b7b065e2477094f4e04e5f4601489578910ef1
phinze <paul.t.hinze@gmail.com>
2009-10-27 15:57:29 -0500

    Handle namespaced strings for :class param

--- lib/factory_girl/factory.rb
+++ lib/factory_girl/factory.rb
@@ -336,7 +336,7 @@ class Factory
 
   def class_for (class_or_to_s)
     if class_or_to_s.respond_to?(:to_sym)
-      Object.const_get(variable_name_to_class_name(class_or_to_s))
+      variable_name_to_class_name(class_or_to_s).split('::').inject(Object) { |o, str| o.const_get(str) }
     else
       class_or_to_s
     end
